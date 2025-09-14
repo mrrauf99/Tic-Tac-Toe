c0310230f8002a0dea0d62bee32a0499ce3ee27e
mrrauf99 <itxrauf99@gmail.com>
2025-09-14 17:09:49 +0500

    Add class highlighted

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -2,7 +2,10 @@ export default function Log({ turns }) {
   return (
     <ol id="log">
       {turns.map((turn) => (
-        <li key={`${turn.square.row}${turn.square.col}`}>
+        <li
+          className="highlighted"
+          key={`${turn.square.row}${turn.square.col}`}
+        >
           {turn.player} selected {turn.square.row},{turn.square.col}
         </li>
       ))}
